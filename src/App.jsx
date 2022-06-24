a0f6929b93d0e196f999945c0b5ba7aabe156b66
TamalRojo <ericmacias1011@gmail.com>
2022-06-24 14:08:13 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+const colorPalette = [
+  '#1A1333', '#262949', '#045459', '#087353', '#15C286', '#ABD96D',
+  '#FBBF54', '#EE6B3B', '#EC0F47', '#A02C5D', '#700460', '#022C7A'
+];
+
+function App() {
+
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam illum, dicta odit harum repudiandae tenetur nisi deleniti voluptate. Dignissimos qui hic distinctio aliquam sed porro debitis repellat odit cumque sapiente.</p>
+
+    </div>
+  )
+}
+
+export default App
